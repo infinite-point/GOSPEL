7fdc967acad49f15b33c7464082518cbf6e6ff9a
Hideko Kawakubo <39298946+infinite-point@users.noreply.github.com>
2018-05-24 19:03:58 +0900

    Delete BIC_GP.R

--- R/BIC_GP.R
+++
@@ -1,8 +0,0 @@
-BIC_GP <- function(Ker_X, Ker_Y, beta){
-  n2 <- dim(Ker_X)[1]
-  res2 <- mean((Ker_Y - Ker_X %*% beta)^2)
-  loglik <- -(n2/2) * (log(2 * pi * res2) + 1)
-  df <- length(table(as.vector(beta))) - 1
-  bic <- -2 * loglik + log(n2) * df
-  return(bic)  
-}
